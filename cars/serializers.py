f2147db0cff6b6e03b7c52ade6b53001db8e8f1f
nthach <nich.thach@hotmail.com>
2022-08-24 08:42:04 -0700

    Created Car Serializer Class and File

---
+++ cars/serializers.py
@@ -0,0 +1,12 @@
+from dataclasses import field, fields
+from rest_framework import serializers
+from .models import Car
+
+
+class CarSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Car
+        fields = ['id', 'make', 'year', 'price']
+
+
+
