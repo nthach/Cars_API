ba5e27fbcc29ebad2516eb7b17e1ad2235f23b5e
nthach <nich.thach@hotmail.com>
2022-08-24 10:09:35 -0700

    Installed Postman interface, Created new Collection + Testing API Request

--- cars/views.py
+++ cars/views.py
@@ -1,12 +1,20 @@
 
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from .serializers import CarSerializer
+from .models import Car
+from cars import serializers
+
 
 @api_view(['GET'])
 def cars_list(request):
 
+    cars = Car.objects.all()
 
-    return Response('ok')
+    serializers = CarSerializer(cars, many=True)
+
+
+    return Response(serializers.data)
 
 
 
