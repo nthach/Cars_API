9e678f899f708a533fc14858ac3f8cfa67a6d6e7
nthach <nich.thach@hotmail.com>
2022-08-25 08:43:07 -0700

    Created New Function for Getting Cars BY Id + Added New URLs Path for car_detail Veiw Function

--- cars/urls.py
+++ cars/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.cars_list),
+    path('<int:pk>/', views.car_detail),
 ]
 
--- cars/views.py
+++ cars/views.py
@@ -7,6 +7,7 @@ from .serializers import CarSerializer
 from .models import Car
 
 
+
 @api_view(['GET', 'POST'])
 def cars_list(request):
     if request.method == 'GET':
@@ -18,6 +19,15 @@ def cars_list(request):
         serializer.is_valid(raise_exception=True)
         serializer.save()
         return Response(serializer.data, status=status.HTTP_201_CREATED) 
+
+@api_view(['GET'])
+def car_detail(request, pk):
+    try:
+        car = Car.objects.get(pk=pk)
+        serializer = CarSerializer(car);
+        return Response(serializer.data);
+    except Car.DoesNotExist:
+        return Response(status=status.HTTP_404_NOT_FOUND);
 
 
 
